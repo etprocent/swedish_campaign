d807bef61960592c63375a6a3540e4026cc9cc54
Jacob Wood <jacob.wood27@gmail.com>
2022-10-15 08:27:34 -0700

    translate a few strings

--- react-app/src/IntroContent.jsx
+++ react-app/src/IntroContent.jsx
@@ -7,7 +7,7 @@ const IntroContent = () => (
     <h1 className="title">Et Procent</h1>
     <p className="intro-para">
       Sverige är ett imponerande exempel på världsscenen genom att anslå 1 % av sina inkomster till att hjälpa behövande. 
-      <strong>Svenska folket vill ha det så.</strong>
+      {" "}<strong>Svenska folket vill ha det så.</strong>
       <br></br>
       <br></br> 
       I en tid av aldrig tidigare skådad internationell kris, med miljoner som riskerar att drabbas av extrem fattigdom, <strong>måste Sverige visa ledarskap</strong>.
