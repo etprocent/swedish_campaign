518a61237d03e62a52979dbbe20e98df6f52cfe0
Jacob Wood <jacob.wood27@gmail.com>
2022-10-16 21:51:27 -0700

    update keys.js

--- emailGenerator/keys.js
+++ emailGenerator/keys.js
@@ -1,27 +1,12 @@
-const religions = [
-  { adj: "Christian", noun: "Christian" },
-  { adj: "Muslim", noun: "Muslim" },
-  { adj: "Hindu", noun: "Hindu" },
-  { adj: "Sikh", noun: "Sikh" },
-  { adj: "Jewish", noun: "Jew" },
-  { adj: "Buddhist", noun: "Buddhist" },
-  { adj: "religious", noun: "person of faith" },
-];
-
 const questionKeys = {
-  supportAid: "gil6UCe4dG9T",
-  motivation: "wKGNjgRDml1H",
-  whichCountry: "MRPxTl6j1QAw",
-  countryLinks: "Z4awe4sDljLR",
-  conservative: "EejpFBEzP9wK",
-  religion: "IdqRPd6SUMVh",
+  supportAid: "aw0eDbnYFM6e",
+  motivation: "mAN4s5AjLA3D",
+  party: "TKqucFyEhWjH",
   meetMp: "vdZgYVyiLE13",
   meetMpDoubleCheck: "UhNb2Z5nqHtb",
   emailAddress: "ghzBmQTQ2npF",
-  homeAddress: "uLPPjjg5B0Bn",
-  postcode: "hgdzZ05GxSAs",
   name: "daZZA6TwyMP5",
   phoneNumber: "lm89BWs2VeY2",
 };
 
-module.exports = { religions, questionKeys, questionKeys };
+module.exports = { questionKeys };
