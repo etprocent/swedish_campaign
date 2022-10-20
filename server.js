540c659cd33b3d13ad2758257ce40392c5ebb5cd
mikolajkniejski <sarchta@protonmail.com>
2022-10-20 15:09:10 +0200

    fix

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use((req, res, next) => {
-  if (process.env.NODE_ENV === "production" || true) {
+  if (process.env.NODE_ENV === "production") {
     if (req.headers["x-forwarded-proto"] !== "https") {
       console.log("forwarding to https");
       return res.redirect("https://" + req.headers.host + req.url);
